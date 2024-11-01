3e7a190369e8220ed41ce5297216db034d3f6884
Mackenzie Anderson <mackenzie@Chuls-MacBook-Pro.local>
2024-11-01 15:04:33 -0700

    added get route to display all items

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,25 +1,16 @@
 const express = require('express');
-const Item = require('../models/itemModel');
+const { 
+    getAllItems,
+    addItem 
+} = require('../controllers/itemController');
 
 const router = express.Router();
 
 // Get all available items on the home page
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all items'});
-});
+router.get('/', getAllItems);
 
-// practice posting to the mongo database via postman
-router.post('/', async (req, res) => {
-    const {title, quantity, price, description, condition, category, seller} = req.body;
-
-    try {
-        const item = await Item.create({title, quantity, price, description, condition, category, seller});
-        res.status(200).json(item);
-    } catch (error) {
-        res.status(400).json({error: error.message});
-        
-    };
-});
+// add new item
+router.post('/', addItem);
 
 
 module.exports = router;
