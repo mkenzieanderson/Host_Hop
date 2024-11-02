1b7f3f26098fcc3253becd5a76bc12a37e97bdad
Mackenzie Anderson <mackenzie@Chuls-MacBook-Pro.local>
2024-11-02 13:17:37 -0700

    added basic return home link and basic CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/home';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar/>
         <div className="pages">
           <Routes>
             <Route
